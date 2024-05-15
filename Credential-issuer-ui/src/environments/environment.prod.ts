863c7751b8608bc809edb217d5d91830db2b35b4
Dmitri Sidorov <dmitri.sidorov@in2.es>
2024-05-15 16:59:10 +0200

    update keycloak url

--- Credential-issuer-ui/src/environments/environment.prod.ts
+++ Credential-issuer-ui/src/environments/environment.prod.ts
@@ -2,7 +2,7 @@ export const environment = {
   production: true,
   loginParams: {
     has_login: true,
-    login_url: 'http://localhost:8088/realms/EAAProvider',
+    login_url: 'http://localhost:8088/realms/CredentialIssuer',
     client_id: 'oidc4vci-wallet-client',
     scope: 'openid profile email offline_access',
     grant_type:'code'
--- Credential-issuer-ui/src/environments/environment.ts
+++ Credential-issuer-ui/src/environments/environment.ts
@@ -6,7 +6,7 @@ export const environment = {
   production: false,
   loginParams: {
     has_login: true,
-    login_url: 'http://localhost:8088/realms/EAAProvider',
+    login_url: 'http://localhost:8088/realms/CredentialIssuer',
     client_id: 'oidc4vci-wallet-client',
     scope: 'openid profile email offline_access',
     grant_type: 'code'
