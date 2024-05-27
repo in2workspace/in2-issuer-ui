c96e2f5cbbc79dde054b7d8ab7bbf43d692b4910
Daniel Guillen <daniel.guillen@col.vueling.com>
2024-05-27 15:25:36 +0200

    environ, env sin el puerto

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -7,7 +7,7 @@ export const environment = {
     scope: window["env"]["scope"] || 'openid profile email offline_access',
     grant_type: window["env"]["grant_type"] || 'code'
   },
-  base_url: window["env"]["base_url"] || 'https://localhost:8082',
+  base_url: window["env"]["base_url"] || 'https://localhost',
   wallet_url: window["env"]["wallet_url"] || 'https://localhost:4200',
   api_base_url: window["env"]["api_base_url"] || '/api/v1/credentials?type=LEARCredentialEmployee',
   credential_offer_url: window["env"]["credential_offer_url"] || '/api/v1/credential-offer'
