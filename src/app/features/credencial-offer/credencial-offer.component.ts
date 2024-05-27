8233e6fff4e682b93420abe886c382f47a1f84ef
Daniel Guillen <daniel.guillen@col.vueling.com>
2024-05-27 14:12:02 +0200

    refactor

--- src/app/features/credencial-offer/credencial-offer.component.ts
+++ src/app/features/credencial-offer/credencial-offer.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { CredentialProcedureService } from 'src/app/core/services/credential-procedure.service';
 import { AlertService } from 'src/app/core/services/alert.service';
 
@@ -13,15 +13,19 @@ export class CredencialOfferComponent implements OnInit {
 
   public constructor(
     private route: ActivatedRoute,
+    private router: Router,
     private credentialProcedureService: CredentialProcedureService,
     private alertService: AlertService
   ) {}
 
   public ngOnInit(): void {
-    this.route.queryParamMap.subscribe(params => {
-      const transactionCode = params.get('transaction_code');
+    this.route.queryParams.subscribe(params => {
+      const transactionCode = params['transaction_code'];
+      console.log('Transaction Code:', transactionCode);
       if (transactionCode) {
         this.getCredentialOffer(transactionCode);
+      } else {
+        this.alertService.showAlert('No transaction code found in URL.', 'error');
       }
     });
   }
@@ -29,8 +33,15 @@ export class CredencialOfferComponent implements OnInit {
   private getCredentialOffer(transactionCode: string): void {
     this.credentialProcedureService.getCredentialOffer(transactionCode).subscribe(
       data => {
+        console.log('Data received from API:', data);
         if (data.qrCode) {
           this.qrCodeData = data.qrCode;
+          console.log('QR Code Data:', this.qrCodeData);
+          this.router.navigate([], {
+            relativeTo: this.route,
+            queryParams: { transaction_code: transactionCode },
+            queryParamsHandling: 'merge'
+          });
         } else {
           this.alertService.showAlert('No QR code available.', 'error');
         }
