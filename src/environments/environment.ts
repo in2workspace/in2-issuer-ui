1b7d5da467edb1d183fef54bedc3226d6410ae0c
Oriol Canadés <oriol.canades@in2.es>
2024-05-28 20:02:36 +0200

    Update local environment

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,13 +2,13 @@ export const environment = {
   production: false,
   loginParams: {
     has_login: true,
-    login_url: 'http://localhost:8088/realms/EAAProvider',
+    login_url: 'http://localhost:7001/realms/CredentialIssuer',
     client_id: 'oidc4vci-wallet-client',
     scope: 'openid profile email offline_access',
     grant_type: 'code'
   },
-  base_url: 'http://localhost',
-  wallet_url: 'http://localhost:4200',
+  base_url: 'http://localhost:8081',
+  wallet_url: 'http://localhost:4202',
   api_base_url: '/api/v1/credentials?type=LEARCredentialEmployee',
   credential_offer_url: '/api/v1/credential-offer'
 };
