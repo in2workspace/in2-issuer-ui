b3d41ce56d3d3d85ba237a3e19e9f601f90d9cd2
Daniel Guillen <daniel.guillen@col.vueling.com>
2024-06-04 16:03:44 +0200

    refactor enviroment

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,14 +1,14 @@
 export const environment = {
   production: false,
   loginParams: {
-    login_url: 'https://localhost:8443/realms/master',
-    // login_url: 'http://localhost:7001/realms/CredentialIssuer',
+    // login_url: 'https://localhost:8443/realms/master',
+    login_url: 'https://localhost:8443/realms/CredentialIssuer',
     client_id: 'account-console',
     scope: 'openid profile email offline_access',
     grant_type: 'code'
   },
-  base_url: 'http://localhost',
+  base_url: 'https://localhost',
   api_base_url: '/api/v1/credentials?type=LEARCredentialEmployee',
   credential_offer_url: '/api/v1/credential-offer',
   procedures_path: '/api/v1/procedures'
-};
+}
