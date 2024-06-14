23a3eb14d7a685b4a70bc8edca7a8371144d9171
alberto rubio <alberto.rubio@in2.es>
2024-06-14 13:14:10 +0200

    fix variables entorno standard

--- src/assets/env.template.js
+++ src/assets/env.template.js
@@ -2,14 +2,14 @@
   window.env = window.env || {};
 
   // Environment variables
-  window["env"]["login_url"] = "${login_url}";
-  window["env"]["client_id"] = "${client_id}";
-  window["env"]["scope"] = "${scope}";
-  window["env"]["grant_type"] = "${grant_type}";
-  window["env"]["base_url"] = "${base_url}";
-  window["env"]["wallet_url"] = "${wallet_url}";
-  window["env"]["procedures"] = "${procedures}";
-  window["env"]["save_credential"] = "${save_credential}";
-  window["env"]["credential_offer_url"] = "${credential_offer_url}";
-  window["env"]["notification"] = "${notification}";
+  window["env"]["login_url"] = "${LOGIN_URL}";
+  window["env"]["client_id"] = "${CLIENT_ID}";
+  window["env"]["scope"] = "${SCOPE}";
+  window["env"]["grant_type"] = "${GRANT_TYPE}";
+  window["env"]["base_url"] = "${BASE_URL}";
+  window["env"]["wallet_url"] = "${WALLET_URL}";
+  window["env"]["procedures"] = "${PROCEDURES}";
+  window["env"]["save_credential"] = "${SAVE_CREDENTIAL}";
+  window["env"]["credential_offer_url"] = "${CREDENTIAL_OFFER_URI}";
+  window["env"]["notification"] = "${NOTIFICATION}";
 })(this);
