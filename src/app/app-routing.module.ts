4e27f07963f6931c8e52f4d714d491af575d881e
alberto rubio <alberto.rubio@in2.es>
2024-07-09 15:57:05 +0200

    add nueva pagina onboarding

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,6 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { AutoLoginPartialRoutesGuard } from 'angular-auth-oidc-client';
 import { HomeComponent } from './features/home/home.component';
 import { CallbackComponent } from './features/callback/callback.component';
+import { CredentialOfferOnboardingComponent } from './features/credential-offer-onboarding/credential-offer-onboarding.component';
 
 const routes: Routes = [
   { path: '', pathMatch: 'full', redirectTo: 'home' },
@@ -33,10 +34,17 @@ const routes: Routes = [
   },
   {
     path: 'credential-offer',
+    component:CredentialOfferOnboardingComponent,
+    
+      
+  },  
+  {
+    path: 'credential-offer-detail',
     loadChildren: () =>
       import('./features/credencial-offer/credencial-offer.module').then(
         (m) => m.CredencialOfferModule
       ),
+      
   },
   { path: 'callback', component: CallbackComponent },
   { path: '**', redirectTo: 'home' }
---
+++ src/app/features/credential-offer-onboarding/credential-offer-onboarding.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { CredentialOfferOnboardingComponent } from './credential-offer-onboarding.component';
+
+describe('CredentialOfferOnboardingComponent', () => {
+  let component: CredentialOfferOnboardingComponent;
+  let fixture: ComponentFixture<CredentialOfferOnboardingComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [CredentialOfferOnboardingComponent]
+    })
+    .compileComponents();
+    
+    fixture = TestBed.createComponent(CredentialOfferOnboardingComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
