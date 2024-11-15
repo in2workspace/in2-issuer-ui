99584aa7d33d0b28b85d51dd303773aaa785839a
javieralvarezin2 <javier.alvarez@in2.es>
2024-11-15 14:14:52 +0100

    wip: policies

--- jest.config.js
+++ jest.config.js
@@ -25,7 +25,7 @@ module.exports = {
     testPathIgnorePatterns: [
       '/node_modules/',
       '/dist/',
-      '/src/app/features/credential-management/components/', //TODO Fix this
+      '/src/app/features/credential-management/components/', //TODO Fix this (añadir archivo de test)
       // '/src/app/app',
       // '/src/app/core/',
       // '/src/app/features/',
