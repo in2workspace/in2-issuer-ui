d61614599675614a6408e2f15ec3b792bd880816
javieralvarezin2 <javier.alvarez@in2.es>
2024-11-25 15:49:25 +0100

    rollback alert dialog

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,17 +2,17 @@ export const environment = {
   production: false,
   loginParams: {
     // login_url: 'https://localhost:8443/realms/master',
-    login_url: 'https://issuer.dome-marketplace-lcl.org/issuer-keycloak/realms/CredentialIssuer',
+    login_url: 'https://localhost:8443/realms/CredentialIssuer',
     client_id: 'account-console',
     scope: 'openid profile email offline_access',
     grant_type: 'code'
   },
-  base_url: 'https://issuer.dome-marketplace-lcl.org',
-  wallet_url: 'https://wallet.dome-marketplace.org',
+  base_url: 'http://localhost:8081',
+  wallet_url: 'http://localhost:4200',
   knowledgebase_url: "https://knowledgebase.dome-marketplace-sbx.org/",
   procedures: '/api/v1/procedures',
   save_credential: '/vci/v1/issuances',
   credential_offer_url: '/api/v1/credential-offer',
   notification: '/api/v1/notifications',
-  //firma_credential: '/api/v1/sign-credential'
+  firma_credential: '/api/v1/sign-credential'
 };
