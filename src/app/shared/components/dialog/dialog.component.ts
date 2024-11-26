21be5324d863fba8b742ac9b924703e3745ba0b0
javieralvarezin2 <javier.alvarez@in2.es>
2024-11-26 08:55:02 +0100

    wip: add angular dialog i18n

--- src/app/shared/components/dialog/dialog.component.ts
+++ src/app/shared/components/dialog/dialog.component.ts
@@ -1,6 +1,5 @@
 import { Component, Inject } from '@angular/core';
 import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
-import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-confirmation-dialog',
@@ -8,15 +7,11 @@ import { TranslateService } from '@ngx-translate/core';
   styleUrls: ['./dialog.component.scss'],
 })
 export class DialogComponent {
-  translatedMessage: string;
 
   constructor(
     public dialogRef: MatDialogRef<DialogComponent>,
     @Inject(MAT_DIALOG_DATA) public data: { messageKey: string },
-    private translate: TranslateService
-  ) {
-    this.translatedMessage = this.translate.instant(data.messageKey);
-  }
+  ) {}
 
   onConfirm(): void {
     this.dialogRef.close(true);
