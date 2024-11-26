ce10785d880c1967b91815aefa6ef10f6f03f014
javieralvarezin2 <javier.alvarez@in2.es>
2024-11-26 08:36:25 +0100

    wip: add angular dialog i18n

--- src/app/shared/components/dialog/dialog.component.ts
+++ src/app/shared/components/dialog/dialog.component.ts
@@ -8,15 +8,11 @@ import { TranslateService } from '@ngx-translate/core';
   styleUrls: ['./dialog.component.scss'],
 })
 export class DialogComponent {
-  translatedMessage: string;
 
   constructor(
     public dialogRef: MatDialogRef<DialogComponent>,
     @Inject(MAT_DIALOG_DATA) public data: { messageKey: string },
-    private translate: TranslateService
-  ) {
-    this.translatedMessage = this.translate.instant(data.messageKey);
-  }
+  ) {}
 
   onConfirm(): void {
     this.dialogRef.close(true);
