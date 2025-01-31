9994eade8c57775b87d20e8a8de5ad363274f11e
AzureAD\DanielaOcampoFiguero <daniela.ocampo@in2.es>
2025-01-31 08:39:47 +0100

    customizing colors in the navbar

--- src/assets/env.js
+++ src/assets/env.js
@@ -15,6 +15,10 @@
   window["env"]["save_credential"] = "${SAVE_CREDENTIAL}";
   window["env"]["credential_offer_url"] = "${CREDENTIAL_OFFER_URI}";
   window["env"]["notification"] = "${NOTIFICATION}";
+  window["env"]["primary"] = "${PRIMARY}";
+  window["env"]["primary_contrast"]= "${PRIMARY_CONTRAST}";
+  window["env"]["secondary"] = "${SECONDARY}";
+  window["env"]["secondary_contrast"]= "${SECONDARY_CONTRAST}"
   //window["env"]["firma_credential"] = "${FIRMA_CREDENTIAL}"; The`firma_credential` variable has been commented out as it was initially intended for the signature functionality,which remains incomplete. This configuration is currently unnecessary for the existing flows but is expected to be reintroduced in the future when the related use case is implemented.
 
 })(this);
