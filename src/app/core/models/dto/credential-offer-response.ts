824b1beb24ae99ca40964b69520a5297e5fac628
roger <roger.miret@in2.es>
2025-02-03 10:39:31 +0100

    fix typo

--- src/app/core/models/dto/credential-offer-response.ts
+++ src/app/core/models/dto/credential-offer-response.ts
@@ -1,5 +1,5 @@
 export interface CredentialOfferResponse {
   credential_offer_uri: string;
   c_transaction_code: string;
-  c_transaction_code_exprires_in: number;
+  c_transaction_code_expires_in: number;
 }
