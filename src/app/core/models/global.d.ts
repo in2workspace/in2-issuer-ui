ae5984febc23bf9089799f07580e814052390253
roger <roger.miret@in2.es>
2025-03-20 16:27:58 +0100

    add global.d.ts with window interface

---
+++ src/app/core/models/global.d.ts
@@ -0,0 +1,25 @@
+// src/types/global.d.ts
+
+interface Window {
+    env: {
+        login_url?: string;
+        login_realm_path?: string;
+        base_url?: string;
+        wallet_url?: string;
+        wallet_url_test?: string;
+        knowledgebase_url?: string;
+        knowledge_wallet_path?: string;
+        profile?: string;
+        procedures?: string;
+        saveCredential?: string;
+        credential_offer_url?: string;
+        notification?: string;
+        sign_credential_url?: string;
+        primary?: string;
+        primary_contrast?: string;
+        secondary?: string;
+        secondary_contrast?: string;
+        logo_src?: string;
+        favicon_src?: string;
+    };
+}
