d05f265b7756c8613701ba2d8104273731741f4b
roger <roger.miret@in2.es>
2025-04-02 15:52:16 +0200

    add comment

--- src/app/core/constants/iam.constants.ts
+++ src/app/core/constants/iam.constants.ts
@@ -1,5 +1,5 @@
 export const IAM_PARAMS = Object.freeze({
     CLIENT_ID: "vc-auth-client",
-    SCOPE: "openid profile email offline_access vc mandator",
+    SCOPE: "openid profile email offline_access vc mandator", //todo check updates
     GRANT_TYPE: "code"
 });
