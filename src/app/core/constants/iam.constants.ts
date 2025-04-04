d2ead57137f6037c1d063615b13e286d1a960118
RubenModamioGarcia <ruben.modamio@in2.es>
2025-04-04 08:15:37 +0200

    add changes

--- src/app/core/constants/iam.constants.ts
+++ src/app/core/constants/iam.constants.ts
@@ -1,5 +1,5 @@
 export const IAM_PARAMS = Object.freeze({
     CLIENT_ID: "vc-auth-client",
-    SCOPE: "openid profile email offline_access vc mandator", //todo check updates
+    SCOPE: "openid profile email offline_access learcredential rol",
     GRANT_TYPE: "code"
 });
