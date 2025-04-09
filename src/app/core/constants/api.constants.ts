339a546ffc3e9fe67284d1f19846a236ca1e70bb
RubenModamioGarcia <ruben.modamio@in2.es>
2025-04-09 13:04:31 +0200

    fix constants

--- src/app/core/constants/api.constants.ts
+++ src/app/core/constants/api.constants.ts
@@ -1,6 +1,6 @@
 export const API = Object.freeze({
     PROCEDURES_PATH: '/api/v1/procedures',
-    SAVE_CREDENTIAL_PATH: '/api/v1/credentials?type=LEARCredentialEmployee',
+    SAVE_CREDENTIAL_PATH: '/vci/v1/issuances',
     CREDENTIAL_OFFER_PATH: '/api/v1/credential-offer',
     NOTIFICATION_PATH: '/api/v1/notifications',
     SIGN_CREDENTIAL_PATH: '/api/v1/retry-sign-credential',
