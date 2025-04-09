d63b8dba529e8ff4ffd872be16a0adb0387f8d52
RubenModamioGarcia <ruben.modamio@in2.es>
2025-04-09 10:46:54 +0200

    fix scope problem

--- src/app/core/constants/iam.constants.ts
+++ src/app/core/constants/iam.constants.ts
@@ -1,5 +1,5 @@
 export const IAM_PARAMS = Object.freeze({
     CLIENT_ID: "vc-auth-client",
-    SCOPE: "openid profile email offline_access learcredential rol",
+    SCOPE: "openid profile email offline_access learcredential role",
     GRANT_TYPE: "code"
 });
