3333e049243bca96e5675643588e1bcbcd513072
RubenModamioGarcia <ruben.modamio@in2.es>
2025-04-30 08:36:19 +0200

    update constants

--- src/app/core/constants/api.constants.ts
+++ src/app/core/constants/api.constants.ts
@@ -1,7 +1,7 @@
 export const API = Object.freeze({
     PROCEDURES_PATH: '/api/v1/procedures',
     SAVE_CREDENTIAL_PATH: '/backoffice/v1/issuances',
-    CREDENTIAL_OFFER_PATH: '/api/v1/credential-offer',
+    CREDENTIAL_OFFER_PATH: '/oid4vci/v1/credential-offer',
     NOTIFICATION_PATH: '/api/v1/notifications',
     SIGN_CREDENTIAL_PATH: '/api/v1/retry-sign-credential',
     WALLET_URL_TEST: 'https://wallet.dome-marketplace-dev2.org',
