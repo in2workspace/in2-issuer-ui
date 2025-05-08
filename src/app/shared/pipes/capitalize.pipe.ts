29870055afca3ac85d23054ff96d50868f210b0e
roger <roger.miret@in2.es>
2025-05-08 13:21:18 +0200

    fix capitalize pipe

--- src/app/shared/pipes/capitalize.pipe.ts
+++ src/app/shared/pipes/capitalize.pipe.ts
@@ -8,7 +8,7 @@ export class CapitalizePipe implements PipeTransform {
 
   public transform(value: string): string {
     if (!value) return '';
-    return value[0].toUpperCase() + value.slice(1).toLowerCase();
+    return value[0].toUpperCase() + value.slice(1);
   }
 
 }
