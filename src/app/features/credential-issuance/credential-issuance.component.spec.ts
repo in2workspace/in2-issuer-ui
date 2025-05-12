06c272e9b9b1d73cb8f1185755d36ace4a6182fc
roger <roger.miret@in2.es>
2025-05-12 10:06:40 +0200

    fix routes

--- src/app/features/credential-issuance/credential-issuance.component.spec.ts
+++ src/app/features/credential-issuance/credential-issuance.component.spec.ts
@@ -1,23 +1,19 @@
-import { ComponentFixture, fakeAsync, TestBed, tick } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { provideHttpClient } from '@angular/common/http';
-import { of } from 'rxjs';
 import { CredentialIssuanceComponent } from './credential-issuance.component';
-import { RouterModule, ActivatedRoute, convertToParamMap } from '@angular/router';
-import { TranslateModule, TranslateService} from '@ngx-translate/core';
+import { RouterModule, ActivatedRoute } from '@angular/router';
+import { TranslateModule, TranslateService } from '@ngx-translate/core';
 import { OidcSecurityService, StsConfigLoader } from "angular-auth-oidc-client";
 import { AuthService } from "../../core/services/auth.service";
 import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { of } from 'rxjs';
 
 (globalThis as any).structuredClone = (obj: any) => JSON.parse(JSON.stringify(obj));
 
 describe('CredentialIssuanceComponent', () => {
   let component: CredentialIssuanceComponent;
   let fixture: ComponentFixture<CredentialIssuanceComponent>;
-
-  let translateService: TranslateService;
-
-  let configService: any;
 
   let oidcSecurityService: {
     checkAuth: jest.Mock,
@@ -32,79 +28,80 @@ describe('CredentialIssuanceComponent', () => {
     idToken: 'dummyIdToken'
   };
 
-  const mockActivatedRoute = {
-    url: of([{ path: 'create-as-signer' }])
-  };
-
-  beforeEach(async () => {
-    configService = { loadConfigs: () => {} };
-
-    oidcSecurityService = {
-      checkAuth: jest.fn().mockReturnValue(of(mockAuthResponse)),
-      authorize: jest.fn(),
-      logoff: jest.fn()
+  function createMockActivatedRoute(path: string): any {
+    return {
+      snapshot: {
+        pathFromRoot: [
+          { url: [] },
+          { url: [] },
+          { url: [{ path }] }
+        ]
+      }
     };
-
-    await TestBed.configureTestingModule({
-    schemas: [CUSTOM_ELEMENTS_SCHEMA],
-    imports: [BrowserAnimationsModule,
-        RouterModule.forRoot([]),
-        TranslateModule.forRoot({}),
-        CredentialIssuanceComponent],
-    providers: [
-        TranslateService,
-        AuthService,
-        { provide: OidcSecurityService, useValue: oidcSecurityService },
-        { provide: StsConfigLoader, useValue: configService },
-        { provide: ActivatedRoute, useValue: mockActivatedRoute },
-        provideHttpClient(),
-    ]
-}).compileComponents();
-
-    fixture = TestBed.createComponent(CredentialIssuanceComponent);
-    component = fixture.componentInstance;
-    translateService = TestBed.inject(TranslateService);
-
-    fixture.detectChanges();
-  });
+  }
 
   afterEach(() => {
     TestBed.resetTestingModule();
     jest.clearAllMocks();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it('should set "asSigner" to true when path includes "create-as-signer"', async () => {
+    await TestBed.configureTestingModule({
+      schemas: [CUSTOM_ELEMENTS_SCHEMA],
+      imports: [
+        BrowserAnimationsModule,
+        RouterModule.forRoot([]),
+        TranslateModule.forRoot(),
+        CredentialIssuanceComponent
+      ],
+      providers: [
+        TranslateService,
+        AuthService,
+        { provide: OidcSecurityService, useValue: {
+          checkAuth: jest.fn().mockReturnValue(of(mockAuthResponse)),
+          authorize: jest.fn(),
+          logoff: jest.fn()
+        }},
+        { provide: StsConfigLoader, useValue: { loadConfigs: () => {} } },
+        { provide: ActivatedRoute, useValue: createMockActivatedRoute('create-as-signer') },
+        provideHttpClient(),
+      ]
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(CredentialIssuanceComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+
+    expect(component.asSigner).toBe(true);
   });
 
-  it('should set "asSigner$" to true when last path segment is "create-as-signer"', fakeAsync(() => {
-    const mockActivatedRoute = TestBed.inject(ActivatedRoute) as any;
-    mockActivatedRoute.url = of([{ path: 'create-as-signer' }]);
-  
+  it('should set "asSigner" to false when path is not "create-as-signer"', async () => {
+    await TestBed.configureTestingModule({
+      schemas: [CUSTOM_ELEMENTS_SCHEMA],
+      imports: [
+        BrowserAnimationsModule,
+        RouterModule.forRoot([]),
+        TranslateModule.forRoot(),
+        CredentialIssuanceComponent
+      ],
+      providers: [
+        TranslateService,
+        AuthService,
+        { provide: OidcSecurityService, useValue: {
+          checkAuth: jest.fn().mockReturnValue(of(mockAuthResponse)),
+          authorize: jest.fn(),
+          logoff: jest.fn()
+        }},
+        { provide: StsConfigLoader, useValue: { loadConfigs: () => {} } },
+        { provide: ActivatedRoute, useValue: createMockActivatedRoute('create') },
+        provideHttpClient(),
+      ]
+    }).compileComponents();
+
     fixture = TestBed.createComponent(CredentialIssuanceComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
-  
-    let asSignerValue: boolean | undefined;
-    component.asSigner$.subscribe(value => (asSignerValue = value));
-    tick();
-    expect(asSignerValue).toBe(true);
-  }));
 
-  it('should set "asSigner$" to false when last path segment is not "create-as-signer"', fakeAsync(() => {
-    const mockActivatedRoute = TestBed.inject(ActivatedRoute) as any;
-    mockActivatedRoute.url = of([{ path: 'create' }]);
-  
-    fixture = TestBed.createComponent(CredentialIssuanceComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  
-    let asSignerValue: boolean | undefined;
-    component.asSigner$.subscribe(value => (asSignerValue = value));
-    tick();
-    expect(asSignerValue).toBe(false);
-  }));
-  
-  
-
+    expect(component.asSigner).toBe(false);
+  });
 });
--- src/app/features/credential-issuance/credential-issuance.component.ts
+++ src/app/features/credential-issuance/credential-issuance.component.ts
@@ -1,25 +1,19 @@
 import { Component, inject } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { TranslatePipe } from '@ngx-translate/core';
-import { Observable } from 'rxjs';
-import { AsyncPipe } from '@angular/common';
 import { FormCredentialComponent } from '../../shared/components/form-credential/form-credential.component';
-import { map } from "rxjs/operators";
 
 @Component({
     selector: 'app-credential-issuance-admin',
     templateUrl: './credential-issuance.component.html',
     standalone: true,
-    imports: [FormCredentialComponent, AsyncPipe, TranslatePipe]
+    imports: [FormCredentialComponent, TranslatePipe]
 })
 export class CredentialIssuanceComponent {
   public readonly route = inject(ActivatedRoute);
 
-  public asSigner$: Observable<boolean> = this.route.url.pipe(
-    map(segments => {
-      const lastSegment = segments[segments.length - 1]?.path;
-      console.log('lastSegment = ' + lastSegment)
-      return lastSegment === 'create-as-signer';
-    })
-  );
+  public asSigner: boolean = this.route.snapshot.pathFromRoot
+    .flatMap(r => r.url)
+    .map(seg => seg.path)
+    .includes('create-as-signer');
 }
