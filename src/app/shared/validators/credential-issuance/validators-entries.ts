3cf0ab61f3632060942db3da8dee9f1263c5ad3a
Aina Gomez <93551599+aina-gomez@users.noreply.github.com>
2026-05-18 08:30:13 +0200

    fix: make serial number optional for LEARCredentialMachine (#147)
    * fix: make serial number optional for LEARCredentialMachine
    
    * docs: update changelog

--- src/app/shared/validators/credential-issuance/validators-entries.ts
+++ src/app/shared/validators/credential-issuance/validators-entries.ts
@@ -33,7 +33,6 @@ export const orgIdValidatorEntries: ValidatorEntryUnion[] = [
 ];
 
 export const serialNumberValidatorEntries: ValidatorEntryUnion[] = [
-  { name: 'required' },
   { name: 'minLength', args: [7] },
   { name: 'maxLength', args: [15] },
   { name: 'pattern', args: ["^[a-zA-Z0-9-]+$"] }
